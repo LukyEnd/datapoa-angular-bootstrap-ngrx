c238f1d2e81e4bd9c16a3282c6577f9839c3faf6
Lucas Sanches <sanchesluka@hotmail.com>
2022-02-01 10:26:09 -0300

    builder error working

--- src/app/store/builder/erro-builder.ts
+++ src/app/store/builder/erro-builder.ts
@@ -2,10 +2,30 @@ import { HttpErrorResponse } from '@angular/common/http';
 
 export class ErrorBuilder {
   static genericError(error: HttpErrorResponse): string {
-    // const errorMessage2 = error?.error?.errors[0]?.suggestedUserActions[0];
-    // if (!!errorMessage2) {
-    // return errorMessage2;
-    // }
-    return 'Tivemos um problema!! Retorne para uma das alternativas Abaixo. Caso nao consiga, entre em contato com o administrador do sistema.';
+    var message =
+      ' Desculpe o Transtorno!! Retorne para uma das alternativas Abaixo.';
+    switch (error.status) {
+      case 200:
+        let erro200 = `Está Tudo Ok, mas aconteceu algum erro com a URL de acesso. Mensagem do Erro: ${error.message}.`;
+        return erro200 + message;
+      case 400:
+        let erro400 = `Erro genérico Mensagem do Erro: ${error.message}.`;
+        return erro400 + message;
+      case 401:
+        let erro401 = `Página restrita, sem credencial válida para acessá-la. Mensagem do Erro: ${error.message}.`;
+        return erro401 + message;
+      case 403:
+        let erro403 = `Navegador não tem permissão para acessar a página. Mensagem do Erro: ${error.message}.`;
+        return erro403 + message;
+      case 404:
+        let erro404 = `URL digitada, não existe. Mensagem do Erro: ${error.message}.`;
+        return erro404 + message;
+      case 500:
+        let erro500 = `Falha interna do servidor. Mensagem do Erro: ${error.message}.`;
+        return erro500 + message;
+      default:
+        let erroDetail = `Erro Desconhecido. Mensagem do Erro: ${error.message}.`;
+        return erroDetail;
+    }
   }
 }
